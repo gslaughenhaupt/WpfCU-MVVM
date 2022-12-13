46b9775ca1deca2a28a54745ca043a2801ced74a
gslaughenhaupt <gary@slaughenhaupt.org>
2022-12-13 10:19:28 -0600

    Init

--- WpfCU-MVVM/App.xaml.cs
+++ WpfCU-MVVM/App.xaml.cs
@@ -5,7 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-
+using WpfCU_MVVM.View;
+using WpfCU_MVVM.ViewModel;
 namespace WpfCU_MVVM
 {
     /// <summary>
@@ -13,5 +14,10 @@ namespace WpfCU_MVVM
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.DataContext = new MainViewModel(e.Args);
+        }
     }
 }
---
+++ WpfCU-MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.TextFormatting;
+using Prism.Mvvm;
+using WpfCU_MVVM.Model;
+
+namespace WpfCU_MVVM.ViewModel
+{
+    public class MainViewModel : BindableBase
+    {
+        private string[] args;
+        private StringBuilder stringBuilder = new StringBuilder();
+        private ObservableCollection<WordUse> _wordCollection = new ObservableCollection<WordUse>();
+        public ObservableCollection<WordUse> WordCollection
+        {
+            get => _wordCollection;
+            set => SetProperty(ref _wordCollection, value);
+        }
+        public ICollectionView WordCollectionView { get; private set; }
+        public MainViewModel(string[] args)
+        {
+            this.args = args;
+            if(args!= null && args.Length > 0)
+            {
+                ReadText(args[0]);
+            }
+        }
+        private void ReadText(string path)
+        {
+            stringBuilder.Clear();
+            using (StreamReader file = new StreamReader(path))
+            {
+                int counter = 0;
+                string ln;
+                
+                while ((ln = file.ReadLine()) != null)
+                {
+                    stringBuilder.Append(" ");
+                    stringBuilder.Append(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+            var words = stringBuilder.ToString().Split();
+        }
+    }
+}
